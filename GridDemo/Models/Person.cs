169931869602e828a18aeec3d19cc2cc309094a1
VelSal <v.salvatore@live.be>
2024-03-19 09:18:36 +0100

    Ajoutez des fichiers projet.

---
+++ GridDemo/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GridDemo.Models
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+
+        public Person(string name, int age, string gender)
+        {
+            Name = name;
+            Age = age;
+            Gender = gender;
+        }
+    }
+}
