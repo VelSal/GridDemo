64157abcaad1417f7aec55b7f66660a9e512d2ef
VelSal <v.salvatore@live.be>
2024-03-19 15:06:41 +0100

    ok

--- GridDemo/Models/Countries.cs
+++ GridDemo/Models/Countries.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace GridDemo.Models
+﻿namespace GridDemo.Models
 {
     public class Countries
     {
